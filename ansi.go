745c2f5a84c79a4b6e1b694620976e657b129e92
Michael Treanor <skeptycal@gmail.com>
2022-03-02 02:00:29 -0600

    (GoBot) devtag version v0.5.0-1646192449161672: ansi colors and parse for filenames

--- ansi.go
+++ ansi.go
@@ -1,16 +1,24 @@
 package types
 
 import (
+	"os"
+
 	"github.com/fatih/color"
 	// . "github.com/fatih/color"
 )
 
+// NoColor defines if the output is colorized or not. It's dynamically set to false or true based on the stdout's file descriptor referring to a terminal or not. It's also set to true if the NO_COLOR environment variable is set (regardless of its value). This is a global option and affects all colors. For more control over each color block use the methods DisableColor() individually.
+var NoColor = color.NoColor
+
+// ANSI color printers
 var (
+	Reset       = NewColor(ResetCode)
+	Canary      = NewColor(FgHiYellow).Add(Bold)
+	Green       = NewColor(FgHiGreen)
+	Whataburger = NewColor(color.Attribute(color.FgRed))
 
-	// NoColor defines if the output is colorized or not. It's dynamically set to false or true based on the stdout's file descriptor referring to a terminal or not. It's also set to true if the NO_COLOR environment variable is set (regardless of its value). This is a global option and affects all colors. For more control over each color block use the methods DisableColor() individually.
-	NoColor = color.NoColor
-
-	Reset = color.Reset
+	ATTN = Canary.Add(BgRed)
+	INFO = Green.Add(Bold).Add(Italic).Add(BgHiBlue)
 )
 
 type (
@@ -26,3 +34,81 @@ type (
 func NewColor(value ...color.Attribute) *color.Color {
 	return color.New(value...)
 }
+
+func Attn(format string, args ...interface{}) {
+	log.Infof(ATTN.Sprintf(format, args...))
+}
+
+var myWriter = os.Stdout
+
+func Blue(format string, args ...interface{}) (n int, err error) {
+	return color.New(color.FgBlue).Fprintf(myWriter, format, args...)
+}
+
+func Info(format string, args ...interface{}) {
+	log.Infof(INFO.Sprintf(format, args...))
+}
+
+// constants from https://github.com/fatih/color
+
+// Base attributes
+const (
+	ResetCode Attribute = iota
+	Bold
+	Faint
+	Italic
+	Underline
+	BlinkSlow
+	BlinkRapid
+	ReverseVideo
+	Concealed
+	CrossedOut
+)
+
+// Foreground text colors
+const (
+	FgBlack Attribute = iota + 30
+	FgRed
+	FgGreen
+	FgYellow
+	FgBlue
+	FgMagenta
+	FgCyan
+	FgWhite
+)
+
+// Foreground Hi-Intensity text colors
+const (
+	FgHiBlack Attribute = iota + 90
+	FgHiRed
+	FgHiGreen
+	FgHiYellow
+	FgHiBlue
+	FgHiMagenta
+	FgHiCyan
+	FgHiWhite
+)
+
+// Background text colors
+const (
+	BgBlack Attribute = iota + 40
+	BgRed
+	BgGreen
+	BgYellow
+	BgBlue
+	BgMagenta
+	BgCyan
+	BgWhite
+)
+
+// Background Hi-Intensity text colors
+const (
+	BgHiBlack Attribute = iota + 100
+	BgHiRed
+	BgHiGreen
+	BgHiYellow
+	BgHiBlue
+	BgHiMagenta
+	BgHiCyan
+	BgHiWhite
+)
--- parse.go
+++ parse.go
@@ -1,7 +1,7 @@
 package types
 
 import (
-	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -34,22 +34,47 @@ type (
 	}
 )
 
-func GetFileList(pattern string) (list []os.FileInfo, err error) {
-	if pattern == "" {
-		pattern = "*"
+func GetGoFileList() (files []string, err error) {
+	return GetFileListBySuffix(".", ".go")
+}
+
+func GetFileListBySuffix(path, suffix string) (files []string, err error) {
+	return getFileList(path, suffix, "", "", false)
+}
+
+func GetFileListByPrefix(path, prefix string) (files []string, err error) {
+	return getFileList(path, "", prefix, "", false)
+}
+
+func GetFileListByName(path, needle string) (files []string, err error) {
+	return getFileList(path, "", "", needle, false)
+}
+
+func GetFileListWithDirectories(path string) (files []string, err error) {
+	return getFileList(path, "", "", "", true)
+}
+
+func getFileList(path, suffix, prefix, needle string, isDir bool) (files []string, err error) {
+	if path == "" {
+		path, _ = os.Getwd()
 	}
+	a, _ := filepath.Abs(path)
 
-	err = filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() && !strings.HasSuffix(info.Name(), "*.go") {
-			list = append(list, info)
-		}
+	err = filepath.WalkDir(a, func(path string, info fs.DirEntry, err error) error {
 
-		fmt.Printf("dir: %v: name: %s\n", info.IsDir(), path)
-		return nil
+		if err == nil {
+			if !isDir && info.IsDir() {
+				return nil
+			}
+			if strings.HasSuffix(path, suffix) {
+				if strings.HasPrefix(path, prefix) {
+					if strings.Contains(path, needle) {
+						files = append(files, path)
+					}
+				}
+			}
+		}
+		return err
 	})
-
 	return
 }
